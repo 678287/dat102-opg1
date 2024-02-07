2439c70b01c0ff2c8971155066094cf8bac113e3
Magne Svalland <142848880+678287@users.noreply.github.com>
2024-02-07 05:31:36 +0100

    Update Filmarkiv2.java

--- dat102/src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
+++ dat102/src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
@@ -3,36 +3,36 @@ package no.hvl.data102.filmarkiv.impl;
 import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
 
 public class Filmarkiv2 implements FilmarkivADT {
-  
-private int antall; // antall filmer 
-private LinearNode<Film> start; // Starten av linære kjeden
 
-	public Filmarkiv2() {                     
-        start = null; // starten av den linære kjeden er null siden ingen filmer er lagt til enda 
-	antall = 0; // antall filmer starter 0
-    }
+	private int antall; // antall filmer
+	private LinearNode<Film> start; // Starten av linære kjeden
 
-        @Override
-	public Film finnFilm(int nr) {
-	    LinearNode<Film> temp = start; // Start fra begynnelsen
-	    while (temp != null) { // Går gjennom kjeden som har blitt fyllt inn
-	        if (temp.data.getFilmnr() == nr) { // vis nummeret du har lagt in stemmer med et nr i kjeden vil den returne filmen
-	            return temp.data;
-	        }
-	        temp = temp.neste; // vis ikke vil den gå til kjeden sin neste node som er fyllet inn
-	    }
-	    return null; // returner null vis filmen ikke er funnet
+	public Filmarkiv2() {
+		start = null; // starten av den linære kjeden er null siden ingen filmer er lagt til enda
+		antall = 0; // antall filmer starter 0
 	}
-	
+
+	@Override
+	public Film finnFilm(int nr) {
+		LinearNode<Film> temp = start; // Start fra begynnelsen
+		while (temp != null) { // Går gjennom kjeden som har blitt fyllt inn
+			if (temp.data.getFilmnr() == nr) { // vis nummeret du har lagt in stemmer med et nr i kjeden vil den returne
+												// filmen
+				return temp.data;
+			}
+			temp = temp.neste; // vis ikke vil den gå til kjeden sin neste node som er fyllet inn
+		}
+		return null; // returner null vis filmen ikke er funnet
+	}
 
 	@Override
 	public void leggTilFilm(Film nyFilm) {
 		LinearNode<Film> ny = new LinearNode<>(nyFilm);// Ny node med den nye filmen
-	        ny.neste = start; // Sett den nye noden som den første i kjeden
-	        start = ny; // Oppdater starten av kjeden til den nye noden
-	        antall++; // Øk antall filmer
-	        System.out.println("Ny film lagt til arkivet");
-	    }
+		ny.neste = start; // Sett den nye noden som den første i kjeden
+		start = ny; // Oppdater starten av kjeden til den nye noden
+		antall++; // Øk antall filmer
+		System.out.println("Ny film lagt til arkivet");
+	}
 
 	@Override
 	public boolean slettFilm(int filmnr) {
@@ -58,13 +58,11 @@ private LinearNode<Film> start; // Starten av linære kjeden
 		while (temp != null) {
 			if (temp.data.getSjanger() == sjanger) {
 				filmer++;
-					}
+			}
 			temp = temp.neste;
 		}
 		return filmer;
-	}			
-		
-	
+	}
 
 	@Override
 	public int antall() {

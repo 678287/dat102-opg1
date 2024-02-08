4fd5727269c23022d56a868144cf35fd56d216a3
Magne Svalland <142848880+678287@users.noreply.github.com>
2024-02-08 17:22:50 +0100

    Update Meny.java

--- dat102/src/no/hvl/data102/filmarkiv/klient/Meny.java
+++ dat102/src/no/hvl/data102/filmarkiv/klient/Meny.java
@@ -8,25 +8,22 @@ public class Meny {
 
 	private Tekstgrensesnitt tekstgr;
 	private FilmarkivADT filmarkiv;
-	
-	public Meny(FilmarkivADT filmarkiv){
-	tekstgr = new Tekstgrensesnitt();
-	
-	this.filmarkiv = filmarkiv;
+
+	public Meny(FilmarkivADT filmarkiv) {
+		tekstgr = new Tekstgrensesnitt();
+
+		this.filmarkiv = filmarkiv;
 	}
-	public void start(){
-		
-		Film shrek = new Film(1, "Andrew Adamson","Shrek", 22062001, Sjanger.KOMEDIE, "Dreamworks" );
-		Film beemovie = new Film(2, "Simon smith","Beemovie", 26122007, Sjanger.KOMEDIE, "Dreamworks");
+
+	public void start() {
+
+		Film shrek = new Film(1, "Andrew Adamson", "Shrek", 22062001, Sjanger.KOMEDIE, "Dreamworks");
+		Film beemovie = new Film(2, "Simon smith", "Beemovie", 26122007, Sjanger.KOMEDIE, "Dreamworks");
 		Film pulpfiction = new Film(3, "Quentin Tarantino", "Pulp fiction", 21101994, Sjanger.ACTION, "Miramax films");
-		
+
 		filmarkiv.leggTilFilm(shrek);
 		filmarkiv.leggTilFilm(beemovie);
 		filmarkiv.leggTilFilm(pulpfiction);
-		
-		
-	// legg inn en del forhåndsdefinerte filmer for å teste metodene
-	// ..
-	// TODO
-   }
+
+	}
 }

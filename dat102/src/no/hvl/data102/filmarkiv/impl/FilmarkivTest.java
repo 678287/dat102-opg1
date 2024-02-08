598774a96e9d8bcc2dd56ce9db61c7b1ad8f3c48
Magne Svalland <142848880+678287@users.noreply.github.com>
2024-02-08 17:21:48 +0100

    Update FilmarkivTest.java

--- dat102/src/no/hvl/data102/filmarkiv/impl/FilmarkivTest.java
+++ dat102/src/no/hvl/data102/filmarkiv/impl/FilmarkivTest.java
@@ -1,41 +1,62 @@
 package no.hvl.data102.filmarkiv.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FilmarkivTest {
 
-	@Test
-	public void test() {
-		Filmarkiv filmarkiv = new Filmarkiv(10);
-		assertEquals(0, filmarkiv.antall());
+	private Filmarkiv filmarkiv;
+	private Film film1;
+	private Film film2;
+
+	@Before
+	public void forAlle() {
+		filmarkiv = new Filmarkiv(10);
+
+		film1 = new Film(1, "Regissør1", "Film1", 2020, Sjanger.ACTION, "Selskap1");
+		film2 = new Film(2, "Regissør2", "Film2", 2020, Sjanger.KOMEDIE, "Selskap2");
+		filmarkiv.leggTilFilm(film1);
+		filmarkiv.leggTilFilm(film2);
 	}
-	
+
 	@Test
 	public void testLeggTilFilm() {
-	    Filmarkiv filmarkiv = new Filmarkiv(10);
-	    assertEquals(0, filmarkiv.antall());
+		assertEquals(2, filmarkiv.antall());
 
-	    Film film1 = new Film(1, "Regissør1", "Film1", 2000, Sjanger.ACTION, "Selskap1");
-	    filmarkiv.leggTilFilm(film1);
+		Film nyFilm = new Film(3, "Regissør3", "Film3", 2002, Sjanger.DRAMA, "Selskap3");
+		filmarkiv.leggTilFilm(nyFilm);
 
-	    assertEquals(1, filmarkiv.antall());
-	    assertEquals(film1, filmarkiv.finnFilm(1));
+		assertEquals(3, filmarkiv.antall());
+		assertEquals(nyFilm, filmarkiv.finnFilm(3));
 	}
 
 	@Test
 	public void testSoekTittel() {
-	    Filmarkiv filmarkiv = new Filmarkiv(10);
-
-	    Film film1 = new Film(1, "Regissør1", "Film1", 2022, Sjanger.ACTION, "Selskap1");
-	    Film film2 = new Film(2, "Regissør2", "Film2", 2023, Sjanger.FANTASY, "Selskap2");
-	    filmarkiv.leggTilFilm(film1);
-	    filmarkiv.leggTilFilm(film2);
-
-	    Film[] result = filmarkiv.soekTittel("Film1");
-	    assertEquals(1, result.length);
-	    assertEquals(film1, result[0]);
+		Film[] result = filmarkiv.soekTittel("Film1");
+		assertEquals(1, result.length);
+		assertEquals(film1, result[0]);
 	}
 
+	@Test
+	public void testSoekProdusent() {
+		Film[] result = filmarkiv.soekProdusent("Regissør2");
+		assertEquals(1, result.length);
+		assertEquals(film2, result[0]);
+	}
+
+	@Test
+	public void testAntallSjanger() {
+		assertTrue(filmarkiv.antall(Sjanger.ACTION) > 0);
+		assertFalse(filmarkiv.antall(Sjanger.FANTASY) < 0);
+		assertTrue(filmarkiv.antall(Sjanger.KOMEDIE) > 0);
+	}
+
+	@Test
+	public void testAntallTotalt() {
+		assertEquals(2, filmarkiv.antall());
+	}
 }

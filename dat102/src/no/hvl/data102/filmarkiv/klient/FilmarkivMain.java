0126b92a91cfd509a671444e3c5a9c78a55c2e89
Magne Svalland <142848880+678287@users.noreply.github.com>
2024-02-08 17:22:21 +0100

    Update FilmarkivMain.java

--- dat102/src/no/hvl/data102/filmarkiv/klient/FilmarkivMain.java
+++ dat102/src/no/hvl/data102/filmarkiv/klient/FilmarkivMain.java
@@ -1,5 +1,7 @@
 package no.hvl.data102.filmarkiv.klient;
 
+import java.util.Arrays;
+
 import no.hvl.data102.filmarkiv.adt.*;
 import no.hvl.data102.filmarkiv.impl.*;
 
@@ -9,9 +11,16 @@ public class FilmarkivMain {
 		FilmarkivADT filma = new Filmarkiv(100);
 		Meny meny = new Meny(filma);
 		meny.start();
+		
+		//tester noen metoder
+		
+		System.out.println("Antall filmer av sjanger komedie: " + filma.antall(Sjanger.KOMEDIE));
+		System.out.println("Finn info om film nr 1: " + filma.finnFilm(1));
+		System.out.println("Søk etter film med tittel: " + Arrays.toString(filma.soekTittel("Beemovie")));
+		System.out.println("Søk produsent: " + Arrays.toString(filma.soekProdusent("Andrew Adamson")));
+		System.out.println("Antall: " + filma.antall());
 
-		//tester metodene
 
-	       System.out.println("Antall filmer av sjanger komedie: " + filma.antall(Sjanger.KOMEDIE));
+
 	}
 }

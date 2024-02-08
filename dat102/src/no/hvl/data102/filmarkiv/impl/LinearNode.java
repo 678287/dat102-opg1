a69276f70c056117540e7a44cac3ca868881a341
Magne Svalland <142848880+678287@users.noreply.github.com>
2024-02-08 17:21:58 +0100

    Update LinearNode.java

--- dat102/src/no/hvl/data102/filmarkiv/impl/LinearNode.java
+++ dat102/src/no/hvl/data102/filmarkiv/impl/LinearNode.java
@@ -1,13 +1,13 @@
 package no.hvl.data102.filmarkiv.impl;
 
 public class LinearNode<T> {
-	
-	 public T data;
-	 public LinearNode<T> neste;
 
-	 public LinearNode(T data) {
-	        this.data = data;
-	        this.neste = null;
-	    }
+	public T data;
+	public LinearNode<T> neste;
+
+	public LinearNode(T data) {
+		this.data = data;
+		this.neste = null;
+	}
 
 }
